53333fe957c0b06c6b741d41b7b98925f8dc8216
Lucas Almeida <almeidalcs.lucas@gmail.com>
2023-09-22 02:25:35 -0300

    feat(LivroForm): Implementado tratamento de ano de publicação válido

--- src/components/LivroForm.tsx
+++ src/components/LivroForm.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import { LivroFormProps } from "../types/LivroFormProps";
 
 const LivroForm: React.FC<LivroFormProps> = ({ livro, onChange, onSubmit }) => {
+  const handleAnoPublicacaoChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { value } = e.target;
+    if (!isNaN(Number(value)) && Number(value) >= 0) {
+      onChange(e);
+    }
+  };
+
 };
 
 export default LivroForm;
