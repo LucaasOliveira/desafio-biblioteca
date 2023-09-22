f1b13c1ebf26dcdb257deb38585d3831be53bd1d
Lucas Almeida <almeidalcs.lucas@gmail.com>
2023-09-22 02:28:46 -0300

    refactor(ListaLivros): Estrutura da lista de livros

--- src/components/ListaLivros.tsx
+++ src/components/ListaLivros.tsx
@@ -6,6 +6,26 @@ const ListaLivros: React.FC<ListaLivrosProps> = ({
   onEdit,
   onDelete
 }) => {
+  return (
+    <div>
+      <h2>Livros Cadastrados</h2>
+      <ul>
+        {livros.map((livro, index) =>
+          <li key={index}>
+            <strong>Título:</strong> {livro.titulo} <br />
+            <strong>Autor:</strong> {livro.autor}
+            <br />
+            <strong>Ano de Publicação:</strong> {livro.anoPublicacao} <br />
+            <strong>Data de Cadastro:</strong> {livro.dataCadastro} <br />
+            <strong>Gênero:</strong> {livro.genero} <br />
+            <strong>Descrição:</strong> {livro.descricao} <br />
+            <button onClick={() => onEdit(livro)}>Editar</button>
+            <button onClick={() => onDelete(livro)}>Excluir</button>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default ListaLivros;
