bcda40c1a9d1177de91603e594fad70c7516164d
Lucas Almeida <almeidalcs.lucas@gmail.com>
2023-09-22 02:07:33 -0300

    feat(GerenciadorDeLivros): Adicionar componente GerenciadorDeLivros

---
+++ src/components/GereciadorDeLivros.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import LivroForm from './LivroForm';
+import ListaLivros from './ListaLivros';
+import EditarLivroModal from './EditarLivroModal';
+import ExcluirLivroModal from './ExcluirLivroModal';
+import { Livro } from '../types/Livro';
+
+const GerenciadorDeLivros: React.FC = () => {
+};
+
+export default GerenciadorDeLivros;
