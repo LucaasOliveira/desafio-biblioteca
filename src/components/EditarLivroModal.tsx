ddedf69af8b1a68167d494166b244d0cdb75436a
Lucas Almeida <almeidalcs.lucas@gmail.com>
2023-09-22 02:30:41 -0300

    feat(EditarLivroModal/ExcluirLivroModal): criar modal para editar e excluir

---
+++ src/components/EditarLivroModal.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Modal from "react-modal";
+import LivroFormEdicao from "./LivroFormEdicao";
+import { EditarLivroModalProps } from "../types/EditarLivroModalProps";
+
+const EditarLivroModal: React.FC<EditarLivroModalProps> = ({
+  isOpen,
+  onClose,
+  livroEmEdicao,
+  onEditChange,
+  onEditSubmit
+}) => {
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onClose}>
+      <div>
+        <h2>Editar Livro</h2>
+        <LivroFormEdicao
+          livro={
+            livroEmEdicao || {
+              id: "",
+              titulo: "",
+              autor: "",
+              anoPublicacao: 0,
+              dataCadastro: "",
+              genero: "",
+              descricao: ""
+            }
+          }
+          onChange={onEditChange}
+          onSubmit={onEditSubmit}
+        />
+        <button onClick={onClose}>Cancelar</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default EditarLivroModal;
