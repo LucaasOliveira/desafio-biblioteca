3ba847415524eb0f29d0bd9c9ee7f6c1a42cf6e9
Lucas Almeida <almeidalcs.lucas@gmail.com>
2023-09-22 02:08:58 -0300

    feat(GerenciadorDeLivros): Adicionar estado para livro e outros

--- src/components/GereciadorDeLivros.tsx
+++ src/components/GereciadorDeLivros.tsx
@@ -7,6 +7,27 @@ import ExcluirLivroModal from './ExcluirLivroModal';
 import { Livro } from '../types/Livro';
 
 const GerenciadorDeLivros: React.FC = () => {
+  const [livro, setLivro] = useState<Livro>({
+    id: uuidv4(),
+    titulo: '',
+    autor: '',
+    anoPublicacao: 0,
+    dataCadastro: new Date().toLocaleDateString(),
+    genero: '',
+    descricao: '',
+  });
+
+  const [livros, setLivros] = useState<Livro[]>([]);
+  const [livroEmEdicao, setLivroEmEdicao] = useState<Livro | null>(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [livroParaExcluir, setLivroParaExcluir] = useState<Livro | null>(null);
+  const [modalExclusaoIsOpen, setModalExclusaoIsOpen] = useState(false);
+
+  useEffect(() => {
+    const livrosArmazenados = JSON.parse(localStorage.getItem('livros') || '[]');
+    setLivros(livrosArmazenados);
+  }, []);
+
 };
 
 export default GerenciadorDeLivros;
