40380193081dbc6d4a07b0deb4706a0318edf4b7
Lucas Almeida <almeidalcs.lucas@gmail.com>
2023-09-24 22:05:54 -0300

    feat: remover modais e estilos que nao vao ser usados

--- src/types/ExcluirLivroModalProps.ts
+++
@@ -1,8 +0,0 @@
-import { Livro } from "./Livro";
-
-export interface ExcluirLivroModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-  livroParaExcluir: Livro | null;
-  onExcluirConfirm: () => void;
-}
